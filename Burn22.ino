cb02e01c1c009efd11fce2be71746eecef6730ce
Daniel Farmer <drayfar@outlook.com>
2022-05-31 17:16:10 -0400

    Add theater chase for one strip (#2)

--- Burn22.ino
+++ Burn22.ino
@@ -1,10 +1,39 @@
+#include <Adafruit_NeoPixel.h>
+
+// Digital pin for LED strip
+#define PIN 53
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(100, PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(25);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(200);
+  theaterChaseRainbow(100);
+}
+
+// https://github.com/adafruit/Adafruit_NeoPixel/blob/0f6c881b919f0e1fba387fbb22100b8d70f5751b/examples/strandtest/strandtest.ino#L123
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
 }
